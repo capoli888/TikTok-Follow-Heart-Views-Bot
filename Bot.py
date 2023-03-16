7785ba806317ccf978d44636bd3919525491b4bf
capoli888 <123908505+capoli888@users.noreply.github.com>
2023-03-17 01:01:44 +0700

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -149,7 +149,7 @@ def loop6():
 
 print("Author: https://github.com/NoNameoN-A")
 
-vidUrl = "https://www.tiktok.com/@github_nonameon/video/6898692248968400130" #Change it
+vidUrl = "https://www.tiktok.com/@siaoxin/video/7211139343258242330" #Change it
 
 bot = int(input("What do you want to do?\n1 - Auto views(500)\n2 - Auto hearts\n3 - Auto (FIRST) comments heart\n4 - Auto followers\n5 - Auto Share\n6 - Simple reload\n"))
 i = 0
@@ -158,7 +158,7 @@ chrome_options = webdriver.ChromeOptions()
 #chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(executable_path=r'/home/nonameon/Desktop/Programmi/TikTok-Follow-Heart-Views-Bot-main/chromedriver',chrome_options=chrome_options) #Change it
+driver = webdriver.Chrome(executable_path=r'C:\Users\USER\Downloads\Programs',chrome_options=chrome_options) #Change it
 
 driver.get("https://vipto.de/")
 
